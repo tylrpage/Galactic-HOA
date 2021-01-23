96d7b834143240ed27d59fae9e69786dc0909d2e
dj46563 <tylrpage@gmail.com>
2021-01-23 10:34:30 -0800

    Created basic movement and animation controller

---
+++ Less is More/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private string _currentAnimationState;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Transform spriteTransform;
+
+    public void SetSpriteDirection(bool right)
+    {
+        Vector3 currentScale = spriteTransform.localScale;
+        if (right)
+        {
+            spriteTransform.localScale = new Vector3(Mathf.Abs(currentScale.x) * -1f, currentScale.y, currentScale.z);
+        }
+        else
+        {
+            spriteTransform.localScale = new Vector3(Mathf.Abs(currentScale.x), currentScale.y, currentScale.z);
+        }
+    }
+
+    public void ChangeAnimationState(string newState)
+    {
+        if (_currentAnimationState == newState) return;
+        
+        animator.Play(newState);
+        _currentAnimationState = newState;
+    }
+}
---
+++ Less is More/Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speedMult;
+
+    private Rigidbody2D _rb;
+    private AnimationController _animationController;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _animationController = GetComponent<AnimationController>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _rb.AddForce(inputDir * speedMult, ForceMode2D.Force);
+        
+        // Animation stuff
+        if (inputDir == Vector2.zero)
+        {
+            _animationController.ChangeAnimationState("idle");
+        }
+        else
+        {
+            _animationController.ChangeAnimationState("run");
+
+            if (inputDir.x > 0)
+            {
+                _animationController.SetSpriteDirection(true);
+            }
+            else if (inputDir.x < 0)
+            {
+                _animationController.SetSpriteDirection(false);
+            }
+        }
+    }
+}
