26a956e3710fe0b517fdfc0cc4cac762aaa1a636
dj46563 <tylrpage@gmail.com>
2021-01-23 19:57:06 -0800

    Leaf blower blows leafs

--- Less is More/Assets/Scripts/LeafBlower.cs
+++ Less is More/Assets/Scripts/LeafBlower.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 
 public class LeafBlower : MonoBehaviour
 {
+    private static readonly int MaxLeafsThatCanBeBlown = 20;
+    
     [SerializeField] private PolygonCollider2D windCollider;
     private Inputs _inputs;
-    
+    private ContactFilter2D _filter;
+
+    private void Awake()
+    {
+        int leafLayer = LayerMask.NameToLayer("Leaf");
+        _filter.layerMask = (1 << leafLayer);
+        _filter.useLayerMask = true;
+    }
+
     public void SetInputs(Inputs inputs)
     {
         _inputs = inputs;
@@ -19,6 +29,18 @@ public class LeafBlower : MonoBehaviour
         {
             windCollider.GetComponent<SpriteRenderer>().enabled = true;
             windCollider.transform.up = _inputs.MouseDir;
+            
+            ContactFilter2D filter = new ContactFilter2D();
+
+            Collider2D[] colliders = new Collider2D[MaxLeafsThatCanBeBlown];
+            int leafCount = windCollider.OverlapCollider(_filter, colliders);
+
+            for (int i = 0; i < leafCount; i++)
+            {
+                Rigidbody2D rb = colliders[i].attachedRigidbody;
+                Vector2 dir = (colliders[i].transform.position - transform.position);
+                rb.AddForce(dir.normalized / dir.magnitude);
+            }
         }
         else
         {
