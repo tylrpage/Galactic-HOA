f74959a8d81662524d9be16341906a142f573317
dj46563 <tylrpage@gmail.com>
2021-01-25 19:08:25 -0800

    Slightly changed perspective on map to allow some side angles. Player prefabs always face towards camera

---
+++ Less is More/Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform _cameraTransform;
+    private Transform _myTransform;
+
+    private void Awake()
+    {
+        _cameraTransform = Camera.main.transform;
+        _myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _myTransform.forward = -(_cameraTransform.position - transform.position);
+    }
+}
